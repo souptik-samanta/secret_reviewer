51e3ccc374f7b4be15b5aa426f8156502545252f
Souptik Samanta <161435060+souptik-samanta@users.noreply.github.com>
2024-09-07 23:02:37 +0530

    Add files via upload

--- Book/redirect.js
+++ Book/redirect.js
@@ -1,3 +1,9 @@
 function redirectToHome() {
     window.location.href = '../index.html'; // Redirect to home page
 }
+function redirectKlumey() {
+    window.location.href = "https://hackclub.slack.com/team/U07EY4D1VGB";
+}
+function redirectAstro() {
+    window.location.href = "https://hackclub.slack.com/team/U07EY4D1VGB";
+}
